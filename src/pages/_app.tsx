94bc4232a8a49882c75dea12a6493164f9aef07f
Mat Brown <matbrown@fastmail.com>
2022-04-12 07:11:52 -0400

    Add RUM

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -2,7 +2,21 @@ import type { AppProps } from "next/app";
 import React from "react";
 import { QueryClient, QueryClientProvider } from "react-query";
 import { Box, ChakraProvider, extendTheme } from "@chakra-ui/react";
+import { datadogRum } from "@datadog/browser-rum";
+
 import { ErrorBoundary } from "../components/ErrorBoundary";
+
+datadogRum.init({
+	applicationId: "f60f57a8-e1db-42cf-93a9-a196425ca832",
+	clientToken: "pubf3c788bd7815c009787b1f14a61c1c57",
+	site: "datadoghq.com",
+	service: "web-client",
+	env: process.env.NODE_ENV,
+	// Specify a version number to identify the deployed version of your application in Datadog
+	// version: '1.0.0',
+	sampleRate: 100,
+	trackInteractions: true,
+});
 
 const queryClient = new QueryClient({
 	defaultOptions: { queries: { useErrorBoundary: true } },
